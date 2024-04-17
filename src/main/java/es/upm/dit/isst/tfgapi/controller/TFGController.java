c279976ef6a1fcbaabe644e2f07235c68d2704c5
Álvaro <alvaro.sanchezm@alumnos.upm.es>
2024-04-18 00:55:17 +0200

    finished backend

--- src/main/java/es/upm/dit/isst/tfgapi/controller/TFGController.java
+++ src/main/java/es/upm/dit/isst/tfgapi/controller/TFGController.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import es.upm.dit.isst.tfgapi.model.Estado;
 import es.upm.dit.isst.tfgapi.model.Sesion;
@@ -22,15 +20,19 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.web.server.ResponseStatusException;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -54,7 +56,7 @@ public class TFGController {
         * @return
         */
     //@CrossOrigin(origins = "http://localhost:8080")
-    
+
     @GetMapping("/tfgs")
     List<TFG> readAll(@RequestParam(name = "tutor", required = false) String tutor) {
         // No me habría hecho falta especificar el nombre del parámetro,
